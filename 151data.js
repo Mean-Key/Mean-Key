6e6088651bf5afcc14ce3da58c86b818dc1fc6e4
Mean-Key <chlalsrl1015@naver.com>
2024-12-11 14:09:59 +0900

    oss 24 Tearm Project

---
+++ 151data.js
@@ -0,0 +1,908 @@
+const POKEDEX = [
+  {
+    NUMBER: 1,
+    NAME: "이상해씨",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_001.png"
+  },
+  {
+    NUMBER: 2,
+    NAME: "이상해풀",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_002.png"
+  },
+  {
+    NUMBER: 3,
+    NAME: "이상해꽃",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_003.png"
+  },
+  {
+    NUMBER: 4,
+    NAME: "파이리",
+    TYPE: "red",
+    FILE: "img/poke151/SV2a_004.png"
+  },
+  {
+    NUMBER: 5,
+    NAME: "리자드",
+    TYPE: "red",
+    FILE: "img/poke151/SV2a_005.png"
+  },
+  {
+    NUMBER: 6,
+    NAME: "리자몽",
+    TYPE: "red",
+    FILE: "img/poke151/SV2a_006.png"
+  },
+  {
+    NUMBER: 7,
+    NAME: "꼬부기",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_007.png"
+  },
+  {
+    NUMBER: 8,
+    NAME: "어니부기",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_008.png"
+  },
+  {
+    NUMBER: 9,
+    NAME: "거북왕",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_009.png"
+  },
+  {
+    NUMBER: 10,
+    NAME: "캐터피",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_010.png"
+  },
+  {
+    NUMBER: 11,
+    NAME: "단데기",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_011.png"
+  },
+  {
+    NUMBER: 12,
+    NAME: "버터플",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_012.png"
+  },
+  {
+    NUMBER: 13,
+    NAME: "뿔충이",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_013.png"
+  },
+  {
+    NUMBER: 14,
+    NAME: "딱충이",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_014.png"
+  },
+  {
+    NUMBER: 15,
+    NAME: "독침붕",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_015.png"
+  },
+  {
+    NUMBER: 16,
+    NAME: "구구",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_016.png"
+  },
+  {
+    NUMBER: 17,
+    NAME: "피죤",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_017.png"
+  },
+  {
+    NUMBER: 18,
+    NAME: "피죤투",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_018.png"
+  },
+  {
+    NUMBER: 19,
+    NAME: "꼬렛",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_019.png"
+  },
+  {
+    NUMBER: 20,
+    NAME: "레트라",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_020.png"
+  },
+  {
+    NUMBER: 21,
+    NAME: "깨비참",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_021.png"
+  },
+  {
+    NUMBER: 22,
+    NAME: "깨비드릴조",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_022.png"
+  },
+  {
+    NUMBER: 23,
+    NAME: "아보",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_023.png"
+  },
+  {
+    NUMBER: 24,
+    NAME: "아보크",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_024.png"
+  },
+  {
+    NUMBER: 25,
+    NAME: "피카츄",
+    TYPE: "yellow",
+    FILE: "img/poke151/SV2a_025.png"
+  },
+  {
+    NUMBER: 26,
+    NAME: "라이츄",
+    TYPE: "yellow",
+    FILE: "img/poke151/SV2a_026.png"
+  },
+  {
+    NUMBER: 27,
+    NAME: "모래두지",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_027.png"
+  },
+  {
+    NUMBER: 28,
+    NAME: "고지",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_028.png"
+  },
+  {
+    NUMBER: 29,
+    NAME: "니드런♀",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_029.png"
+  },
+  {
+    NUMBER: 30,
+    NAME: "니드리나",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_030.png"
+  },
+  {
+    NUMBER: 31,
+    NAME: "니드퀸",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_031.png"
+  },
+  {
+    NUMBER: 32,
+    NAME: "니드런♂",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_032.png"
+  },
+  {
+    NUMBER: 33,
+    NAME: "니드리노",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_033.png"
+  },
+  {
+    NUMBER: 34,
+    NAME: "니드킹",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_034.png"
+  },
+  {
+    NUMBER: 35,
+    NAME: "삐삐",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_035.png"
+  },
+  {
+    NUMBER: 36,
+    NAME: "픽시",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_036.png"
+  },
+  {
+    NUMBER: 37,
+    NAME: "식스테일",
+    TYPE: "red",
+    FILE: "img/poke151/SV2a_037.png"
+  },
+  {
+    NUMBER: 38,
+    NAME: "나인테일",
+    TYPE: "red",
+    FILE: "img/poke151/SV2a_038.png"
+  },
+  {
+    NUMBER: 39,
+    NAME: "푸린",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_039.png"
+  },
+  {
+    NUMBER: 40,
+    NAME: "푸크린",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_040.png"
+  },
+  {
+    NUMBER: 41,
+    NAME: "주뱃",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_041.png"
+  },
+  {
+    NUMBER: 42,
+    NAME: "골뱃",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_042.png"
+  },
+  {
+    NUMBER: 43,
+    NAME: "뚜벅쵸",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_043.png"
+  },
+  {
+    NUMBER: 44,
+    NAME: "냄새꼬",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_044.png"
+  },
+  {
+    NUMBER: 45,
+    NAME: "라플레시아",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_045.png"
+  },
+  {
+    NUMBER: 46,
+    NAME: "파라스",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_046.png"
+  },
+  {
+    NUMBER: 47,
+    NAME: "파라섹트",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_047.png"
+  },
+  {
+    NUMBER: 48,
+    NAME: "콘팡",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_048.png"
+  },
+  {
+    NUMBER: 49,
+    NAME: "도나리",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_049.png"
+  },
+  {
+    NUMBER: 50,
+    NAME: "디그다",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_050.png"
+  },
+  {
+    NUMBER: 51,
+    NAME: "닥트리오",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_051.png"
+  },
+  {
+    NUMBER: 52,
+    NAME: "나옹",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_052.png"
+  },
+  {
+    NUMBER: 53,
+    NAME: "페르시온",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_053.png"
+  },
+  {
+    NUMBER: 54,
+    NAME: "고라파덕",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_054.png"
+  },
+  {
+    NUMBER: 55,
+    NAME: "골덕",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_055.png"
+  },
+  {
+    NUMBER: 56,
+    NAME: "망키",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_056.png"
+  },
+  {
+    NUMBER: 57,
+    NAME: "성원숭",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_057.png"
+  },
+  {
+    NUMBER: 58,
+    NAME: "가디",
+    TYPE: "red",
+    FILE: "img/poke151/SV2a_058.png"
+  },
+  {
+    NUMBER: 59,
+    NAME: "윈디",
+    TYPE: "red",
+    FILE: "img/poke151/SV2a_059.png"
+  },
+  {
+    NUMBER: 60,
+    NAME: "발챙이",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_060.png"
+  },
+  {
+    NUMBER: 61,
+    NAME: "슈륙챙이",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_061.png"
+  },
+  {
+    NUMBER: 62,
+    NAME: "강챙이",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_062.png"
+  },
+  {
+    NUMBER: 63,
+    NAME: "캐이시",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_063.png"
+  },
+  {
+    NUMBER: 64,
+    NAME: "윤겔라",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_064.png"
+  },
+  {
+    NUMBER: 65,
+    NAME: "후딘",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_065.png"
+  },
+  {
+    NUMBER: 66,
+    NAME: "알통몬",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_066.png"
+  },
+  {
+    NUMBER: 67,
+    NAME: "근육몬",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_067.png"
+  },
+  {
+    NUMBER: 68,
+    NAME: "괴력몬",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_068.png"
+  },
+  {
+    NUMBER: 69,
+    NAME: "모다피",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_069.png"
+  },
+  {
+    NUMBER: 70,
+    NAME: "우츠동",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_070.png"
+  },
+  {
+    NUMBER: 71,
+    NAME: "우츠보트",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_071.png"
+  },
+  {
+    NUMBER: 72,
+    NAME: "왕눈해",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_072.png"
+  },
+  {
+    NUMBER: 73,
+    NAME: "독파리",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_073.png"
+  },
+  {
+    NUMBER: 74,
+    NAME: "꼬마돌",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_074.png"
+  },
+  {
+    NUMBER: 75,
+    NAME: "데구리",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_075.png"
+  },
+  {
+    NUMBER: 76,
+    NAME: "딱구리",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_076.png"
+  },
+  {
+    NUMBER: 77,
+    NAME: "포니타",
+    TYPE: "red",
+    FILE: "img/poke151/SV2a_077.png"
+  },
+  {
+    NUMBER: 78,
+    NAME: "날쌩마",
+    TYPE: "red",
+    FILE: "img/poke151/SV2a_078.png"
+  },
+  {
+    NUMBER: 79,
+    NAME: "야돈",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_079.png"
+  },
+  {
+    NUMBER: 80,
+    NAME: "야도란",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_080.png"
+  },
+  {
+    NUMBER: 81,
+    NAME: "코일",
+    TYPE: "yellow",
+    FILE: "img/poke151/SV2a_081.png"
+  },
+  {
+    NUMBER: 82,
+    NAME: "레어코일",
+    TYPE: "yellow",
+    FILE: "img/poke151/SV2a_082.png"
+  },
+  {
+    NUMBER: 83,
+    NAME: "파오리",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_083.png"
+  },
+  {
+    NUMBER: 84,
+    NAME: "두두",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_084.png"
+  },
+  {
+    NUMBER: 85,
+    NAME: "두트리오",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_085.png"
+  },
+  {
+    NUMBER: 86,
+    NAME: "쥬쥬",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_086.png"
+  },
+  {
+    NUMBER: 87,
+    NAME: "쥬레곤",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_087.png"
+  },
+  {
+    NUMBER: 88,
+    NAME: "질퍽이",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_088.png"
+  },
+  {
+    NUMBER: 89,
+    NAME: "질뻐기",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_089.png"
+  },
+  {
+    NUMBER: 90,
+    NAME: "셀러",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_090.png"
+  },
+  {
+    NUMBER: 91,
+    NAME: "파르셀",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_091.png"
+  },
+  {
+    NUMBER: 92,
+    NAME: "고오스",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_092.png"
+  },
+  {
+    NUMBER: 93,
+    NAME: "고우스트",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_093.png"
+  },
+  {
+    NUMBER: 94,
+    NAME: "팬텀",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_094.png"
+  },
+  {
+    NUMBER: 95,
+    NAME: "롱스톤",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_095.png"
+  },
+  {
+    NUMBER: 96,
+    NAME: "슬리프",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_096.png"
+  },
+  {
+    NUMBER: 97,
+    NAME: "슬리퍼",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_097.png"
+  },
+  {
+    NUMBER: 98,
+    NAME: "크랩",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_098.png"
+  },
+  {
+    NUMBER: 99,
+    NAME: "킹크랩",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_099.png"
+  },
+  {
+    NUMBER: 100,
+    NAME: "찌리리공",
+    TYPE: "yellow",
+    FILE: "img/poke151/SV2a_100.png"
+  },
+  {
+    NUMBER: 101,
+    NAME: "붐볼",
+    TYPE: "yellow",
+    FILE: "img/poke151/SV2a_101.png"
+  },
+  {
+    NUMBER: 102,
+    NAME: "아라리",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_102.png"
+  },
+  {
+    NUMBER: 103,
+    NAME: "나시",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_103.png"
+  },
+  {
+    NUMBER: 104,
+    NAME: "탕구리",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_104.png"
+  },
+  {
+    NUMBER: 105,
+    NAME: "텅구리",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_105.png"
+  },
+  {
+    NUMBER: 106,
+    NAME: "시라소몬",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_106.png"
+  },
+  {
+    NUMBER: 107,
+    NAME: "홍수몬",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_107.png"
+  },
+  {
+    NUMBER: 108,
+    NAME: "내루미",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_108.png"
+  },
+  {
+    NUMBER: 109,
+    NAME: "또가스",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_109.png"
+  },
+  {
+    NUMBER: 110,
+    NAME: "또도가스",
+    TYPE: "black",
+    FILE: "img/poke151/SV2a_110.png"
+  },
+  {
+    NUMBER: 111,
+    NAME: "뿔카노",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_111.png"
+  },
+  {
+    NUMBER: 112,
+    NAME: "코뿌리",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_112.png"
+  },
+  {
+    NUMBER: 113,
+    NAME: "럭키",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_113.png"
+  },
+  {
+    NUMBER: 114,
+    NAME: "덩쿠리",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_114.png"
+  },
+  {
+    NUMBER: 115,
+    NAME: "캥카",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_115.png"
+  },
+  {
+    NUMBER: 116,
+    NAME: "쏘드라",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_116.png"
+  },
+  {
+    NUMBER: 117,
+    NAME: "시드라",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_117.png"
+  },
+  {
+    NUMBER: 118,
+    NAME: "콘치",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_118.png"
+  },
+  {
+    NUMBER: 119,
+    NAME: "왕콘치",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_119.png"
+  },
+  {
+    NUMBER: 120,
+    NAME: "별가사리",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_120.png"
+  },
+  {
+    NUMBER: 121,
+    NAME: "아쿠스타",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_121.png"
+  },
+  {
+    NUMBER: 122,
+    NAME: "마임맨",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_122.png"
+  },
+  {
+    NUMBER: 123,
+    NAME: "스라크",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_123.png"
+  },
+  {
+    NUMBER: 124,
+    NAME: "루주라",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_124.png"
+  },
+  {
+    NUMBER: 125,
+    NAME: "에레브",
+    TYPE: "yellow",
+    FILE: "img/poke151/SV2a_125.png"
+  },
+  {
+    NUMBER: 126,
+    NAME: "마그마",
+    TYPE: "red",
+    FILE: "img/poke151/SV2a_126.png"
+  },
+  {
+    NUMBER: 127,
+    NAME: "쁘사이저",
+    TYPE: "green",
+    FILE: "img/poke151/SV2a_127.png"
+  },
+  {
+    NUMBER: 128,
+    NAME: "켄타로스",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_128.png"
+  },
+  {
+    NUMBER: 129,
+    NAME: "잉어킹",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_129.png"
+  },
+  {
+    NUMBER: 130,
+    NAME: "갸라도스",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_130.png"
+  },
+  {
+    NUMBER: 131,
+    NAME: "라프라스",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_131.png"
+  },
+  {
+    NUMBER: 132,
+    NAME: "메타몽",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_132.png"
+  },
+  {
+    NUMBER: 133,
+    NAME: "이브이",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_133.png"
+  },
+  {
+    NUMBER: 134,
+    NAME: "샤미드",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_134.png"
+  },
+  {
+    NUMBER: 135,
+    NAME: "쥬피썬더",
+    TYPE: "yellow",
+    FILE: "img/poke151/SV2a_135.png"
+  },
+  {
+    NUMBER: 136,
+    NAME: "부스터",
+    TYPE: "red",
+    FILE: "img/poke151/SV2a_136.png"
+  },
+  {
+    NUMBER: 137,
+    NAME: "폴리곤",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_137.png"
+  },
+  {
+    NUMBER: 138,
+    NAME: "암나이트",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_138.png"
+  },
+  {
+    NUMBER: 139,
+    NAME: "암스타",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_139.png"
+  },
+  {
+    NUMBER: 140,
+    NAME: "투구",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_140.png"
+  },
+  {
+    NUMBER: 141,
+    NAME: "투구푸스",
+    TYPE: "brown",
+    FILE: "img/poke151/SV2a_141.png"
+  },
+  {
+    NUMBER: 142,
+    NAME: "프테라",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_142.png"
+  },
+  {
+    NUMBER: 143,
+    NAME: "잠만보",
+    TYPE: "white",
+    FILE: "img/poke151/SV2a_143.png"
+  },
+  {
+    NUMBER: 144,
+    NAME: "프리져",
+    TYPE: "blue",
+    FILE: "img/poke151/SV2a_144.png"
+  },
+  {
+    NUMBER: 145,
+    NAME: "썬더",
+    TYPE: "yellow",
+    FILE: "img/poke151/SV2a_145.png"
+  },
+  {
+    NUMBER: 146,
+    NAME: "파이어",
+    TYPE: "red",
+    FILE: "img/poke151/SV2a_146.png"
+  },
+  {
+    NUMBER: 147,
+    NAME: "미뇽",
+    TYPE: "gold",
+    FILE: "img/poke151/SV2a_147.png"
+  },
+  {
+    NUMBER: 148,
+    NAME: "신뇽",
+    TYPE: "gold",
+    FILE: "img/poke151/SV2a_148.png"
+  },
+  {
+    NUMBER: 149,
+    NAME: "망나뇽",
+    TYPE: "gold",
+    FILE: "img/poke151/SV2a_149.png"
+  },
+  {
+    NUMBER: 150,
+    NAME: "뮤츠",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_150.png"
+  },
+  {
+    NUMBER: 151,
+    NAME: "뮤",
+    TYPE: "pink",
+    FILE: "img/poke151/SV2a_151.png"
+  }
+]
